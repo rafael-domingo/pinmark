86f9bcef333e4cf1f468b8b7e150aeca977c8262
Rafael Domingo <38447983+rafael-domingo@users.noreply.github.com>
2022-09-30 23:33:29 -0500

    Initial setup of Redux store

--- GoogleMaps.js
+++ GoogleMaps.js
@@ -1,11 +1,13 @@
-import { Client } from "@googlemaps/google-maps-services-js";
 require('dotenv').config();
+const { Client } = require("@googlemaps/google-maps-services-js");
 const express = require('express');
 const router = express.Router();
 const api_key = process.env.GOOGLE_MAPS_API_KEY;
 const client = new Client({});
 
-router.post('/autocomplete', async(req, res) => {
+router.post('/autocomplete', async (req, res) => {
+    console.log('hello')
+    console.log(req.body)
     const searchTerm = req.body.searchTerm;
     const sessionToken = req.body.sessionToken;
     client.placeAutocomplete({
@@ -13,10 +15,11 @@ router.post('/autocomplete', async(req, res) => {
             input: searchTerm,
             key: api_key,
             sessiontoken: sessionToken,
-            types: '(cities'
+            types: '(cities)'
         }
-    }).then(response => {
-        res.json(response.date);
+    }).then(response => {        
+        
+        res.json(response.data);
     }).catch(e => {
         console.log(e);
     })
@@ -45,6 +48,8 @@ router.post('/placeSearch', async (req, res) => {
         params: {
             input: searchTerm,
             inputtype: 'textquery',
+            key: api_key,
+            fields: 'name,icon,photo,place_id,formatted_address,business_status,icon',
             locationbias: `circle:1000@${location.lat},${location.lng}`
         }
     }).then(response => {
