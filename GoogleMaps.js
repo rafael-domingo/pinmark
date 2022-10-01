3f016f4e4e63748f99e074761f70b53a16e676d9
Rafael Domingo <38447983+rafael-domingo@users.noreply.github.com>
2022-09-30 22:05:36 -0500

    First implementation of nodeJS Google Maps API

--- GoogleMaps.js
+++ GoogleMaps.js
@@ -1,4 +1,61 @@
 import { Client } from "@googlemaps/google-maps-services-js";
-
+require('dotenv').config();
+const express = require('express');
+const router = express.Router();
+const api_key = process.env.GOOGLE_MAPS_API_KEY;
 const client = new Client({});
+
+router.post('/autocomplete', async(req, res) => {
+    const searchTerm = req.body.searchTerm;
+    const sessionToken = req.body.sessionToken;
+    client.placeAutocomplete({
+        params: {
+            input: searchTerm,
+            key: api_key,
+            sessiontoken: sessionToken,
+            types: '(cities'
+        }
+    }).then(response => {
+        res.json(response.date);
+    }).catch(e => {
+        console.log(e);
+    })
+})
+
+router.post('/placeDetails', async (req, res) => {
+    const placeId = req.body.placeId;
+    const sessionToken = req.body.sessionToken;
+    client.placeDetails({
+        params: {
+            place_id: placeId,
+            key: api_key,
+            sessiontoken: sessionToken
+        }
+    }).then(response => {
+        res.json(response.data);
+    }).catch(e => {
+        console.log(e);
+    })
+})
+
+router.post('/placeSearch', async (req, res) => {
+    const searchTerm = req.body.searchTerm;
+    const location = req.body.location;
+    client.findPlaceFromText({
+        params: {
+            input: searchTerm,
+            inputtype: 'textquery',
+            locationbias: `circle:1000@${location.lat},${location.lng}`
+        }
+    }).then(response => {
+        res.json(response.data);
+    }).catch(e => {
+        console.log(e);
+    })
+})
+
+module.exports = router;
+
+
+
 
