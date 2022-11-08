16807078e8919c27a7d058bcda73e8777f226cb3
Rafael Domingo <38447983+rafael-domingo@users.noreply.github.com>
2022-11-07 22:45:31 -0600

    Add account information to AccountModal

--- pinmark-app/src/modals/AccountModal.js
+++ pinmark-app/src/modals/AccountModal.js
@@ -9,12 +9,10 @@ import {
     MDBListGroupItem,
     MDBBtn,
     MDBIcon,
-    MDBModal,
-    MDBModalFooter,
-    MDBModalDialog,
-    MDBModalContent,    
+    MDBCard,
+    MDBCardBody   
 } from 'mdb-react-ui-kit';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { resetState } from '../redux/pinmarkSlice';
 import { resetUserState } from '../redux/userSlice';
 import { resetSharedState } from '../redux/sharedSlice';
@@ -22,7 +20,7 @@ import { Navigate } from 'react-router-dom';
 import { signUserOut } from '../util/Firebase';
 
 function AccountModal({handleCloseModal, handleDeleteAccountModal}) {
-    
+    const userState = useSelector((state) => state.user);
     const dispatch = useDispatch();
 
     const handleSignOut = () => {
@@ -43,31 +41,62 @@ function AccountModal({handleCloseModal, handleDeleteAccountModal}) {
             </MDBRow>
         </MDBModalHeader>
         <MDBModalBody style={{minHeight: '500px'}}>
-            <MDBListGroup>
-                <MDBListGroupItem>                    
+            <MDBRow>
+                <MDBCol size={12}>                
+                    <MDBCard>
+                    <MDBCardBody>
+                        <div className='d-flex align-items-center'>
+                            {
+                                userState.profilePicture && (
+                                    <img
+                                        src={userState.profilePicture}
+                                        alt=''
+                                        style={{ width: '45px', height: '45px' }}
+                                        className='rounded-circle'
+                                    />
+                                )
+                            }
+                            {
+                                !userState.profilePicture && (
+                                    <MDBIcon
+                                        icon='user-circle'
+                                        size='3x'                                   
+                                    />
+                                )
+                            }
+                       
+                        <div className='ms-3 d-flex justify-content-start flex-wrap'>
+                            <p className='fw-bold mb-1 w-100' style={{textAlign: 'left'}}>{userState.userName}</p>
+                            <p className='text-muted mb-0 w-100' style={{textAlign: 'left'}}>{userState.email}</p>
+                        </div>
+                        </div>
+                    </MDBCardBody>
+                    </MDBCard>
+                </MDBCol>
+            </MDBRow>
+            <MDBRow style={{marginTop: '20px'}}>
+                <MDBCol size={12} className='d-flex justify-content-end'>                  
                     <MDBBtn 
                         onClick={() => handleSignOut()} 
                         tag='a' 
-                        color='none' 
-                        className='text-dark m-1 d-flex justify-content-between align-items-center' 
-                        style={{padding: 10}}>
+                        color='light' 
+                        className='text-dark' 
+                        style={{padding: 10, margin: 10}}>
                         Sign Out
-                        <MDBIcon icon='sign-out-alt'/>
-                    </MDBBtn>
-                </MDBListGroupItem>
-                <MDBListGroupItem color='danger'>                    
+                        <MDBIcon style={{margin: 10}} color='primary' icon='sign-out-alt'/>
+                    </MDBBtn>                                
                     <MDBBtn 
                         onClick={() => handleDeleteAccountModal(true)} 
                         tag='a'
-                        color='none' 
-                        className='text-dark m-1 d-flex justify-content-between align-items-center' 
-                        style={{padding: 10}}>
+                        color='light' 
+                        className='text-dark' 
+                        style={{padding: 10, margin: 10}}>
                         Delete Account
-                        <MDBIcon icon='user-times'/>
-                    </MDBBtn>
-                </MDBListGroupItem>
-            </MDBListGroup>
-            
+                        <MDBIcon style={{margin: 10}} color='danger' icon='user-times'/>
+                    </MDBBtn>                       
+                </MDBCol>
+            </MDBRow>
+           
         </MDBModalBody>
         <MDBBtn 
             size='lg' 
