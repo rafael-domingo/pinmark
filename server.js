cb3c94d9dc24be1f58e1fa78e5d1de32eb7c7d19
Rafael Domingo <38447983+rafael-domingo@users.noreply.github.com>
2022-11-10 22:27:55 -0600

    Fix for 404 Error

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use('/GoogleMaps', googleMaps);
 
 // Anything that doesn't match the above, send back index.html
 app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/client/build/index.html'))
+    res.sendFile(path.join(__dirname + '/pinmark-app/build/index.html'))
   })
 
 app.listen(port, () => console.log(`Server started on ${port}`));
